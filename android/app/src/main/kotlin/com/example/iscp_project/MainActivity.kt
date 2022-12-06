72376ade6733cb77d71c4ca26b3993a335dffba4
haidar <haidar.dod@gmail.com>
2022-12-06 12:47:02 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/iscp_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iscp_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0XFF032A3B);
+Color kSecondaryColor = const Color(0XFFE5CDA9);
+Color kButtonColor = const Color(0XFF1F5476);
+Color kGreenColor = const Color(0XFF448766).withOpacity(0.7);
+Color kRedColor = const Color(0XFFC06161).withOpacity(0.7);
+Color kLightGreenColor = const Color(0XFF40EE52);
+
+TextStyle primaryTextStyle = GoogleFonts.lexendZetta(
+  color: kPrimaryColor,
+);
